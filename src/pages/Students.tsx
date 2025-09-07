aa26a9a56be748872dadf9af0aa2307ab1e61a58
Victor Azambuja <116668845+buja23@users.noreply.github.com>
2025-09-07 00:39:28 -0300

    Updated Students.tsx

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -102,7 +102,6 @@ export const Students: React.FC = () => {
           )}
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
           {filteredStudents.map((student) => (
             <StudentCard
